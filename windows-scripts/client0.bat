1ee3e1c770159348bc8a716d50faac378b6522cb
Ludovic Barman <ludovic.barman@gmail.com>
2015-10-21 15:10:08 +0200

    Removes the pause at the end of the scripts$

--- windows-scripts/client0.bat
+++ windows-scripts/client0.bat
@@ -1,4 +1,3 @@
 @echo off
 cd ..\\dissent\\
 go run main.go config.go relay.go -client=0 -socks=false
-pause
--- windows-scripts/trustee0.bat
+++ windows-scripts/trustee0.bat
@@ -1,4 +1,3 @@
 @echo off
 cd ..\\dissent\\
 go run main.go config.go relay.go -trustee=0
-pause
