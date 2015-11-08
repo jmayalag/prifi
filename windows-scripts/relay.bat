0aa75d60ff810807240a394520306ad9eaadd4b6
Ludovic Barman <ludovic.barman@gmail.com>
2015-11-08 12:18:48 +0100

    Tests with 3/2 config, working

--- windows-scripts/relay.bat
+++ windows-scripts/relay.bat
@@ -1,4 +1,4 @@
 @echo off
 cd ..\\dissent\\
-go run main.go config.go client.go relay.go relaySocks.go trusteeServer.go -relay -nclients=1 -ntrustees=1 -t1host=localhost:9000 -t2host=localhost:9000 -reportlimit=10
+go run main.go config.go client.go relay.go relaySocks.go trusteeServer.go -relay -t1host=localhost:9000 -t2host=localhost:9000 -reportlimit=10
 pause
