472b44df583b2154bab7dc37c86249ce912f6ef7
Ludovic Barman <ludovic.barman@gmail.com>
2015-12-10 16:05:41 +0100

    New scripts

--- scripts-deterlab/localSinkRun.sh
+++ scripts-deterlab/localSinkRun.sh
@@ -5,6 +5,7 @@ pkill -f ${programFreeBSD}
 
 echo "Deleting old log file ${logPath}${logsinkname}"
 rm -f "${logPath}${logsinkname}"
+rm -f "${nohupoutfolder}${nohupsinkname}${nohupext}"
 
 echo "Starting log sink -logpath=$logPath, log redirected to ${nohupoutfolder}${nohupsinkname}${nohupext}..."
 nohup "${programpath}${programFreeBSD}" -logsink -logpath=$logPath 1>>${nohupoutfolder}${nohupsinkname}${nohupext} 2>&1 &
--- scripts-deterlab/remoteKillAll.sh
+++ scripts-deterlab/remoteKillAll.sh
@@ -6,15 +6,21 @@ maxclient=9
 
 echo "Remoting inside relay"
 ssh router.LB-LLD.SAFER.isi.deterlab.net "pkill -f ${program}"
+ssh router.LB-LLD.SAFER.isi.deterlab.net "rm -rf ${nohupoutfolder}${nohuprelayname}${nohupext}"
+ssh router.LB-LLD.SAFER.isi.deterlab.net "rm -rf ${logPath}relay.out"
 
-# Start clients and client
+# Start clients
 for i in $(seq 0 $maxclient); do
   echo "Remoting inside client-$i"
   ssh client-$i.LB-LLD.SAFER.isi.deterlab.net "pkill -f ${program}"
+  ssh client-$i.LB-LLD.SAFER.isi.deterlab.net "rm -rf ${nohupoutfolder}${nohupclientname}${id}${nohupext}"
+  ssh client-$i.LB-LLD.SAFER.isi.deterlab.net "rm -rf ${logPath}client${i}.out"
 done
 
-# Start clients and trustees
+# Start trustees
 for i in $(seq 0 $maxtrustee); do
   echo "Remoting inside trustee-$i"
   ssh trustee-$i.LB-LLD.SAFER.isi.deterlab.net "pkill -f ${program}"
+  ssh trustee-$i.LB-LLD.SAFER.isi.deterlab.net "rm -rf ${nohupoutfolder}${nohuprelayname}${nohupext}"
+  ssh trustee-$i.LB-LLD.SAFER.isi.deterlab.net "rm -rf ${logPath}trusteeServer.log"
 done
