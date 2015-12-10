ab65b413400f7d5df2080dc3f04a68bdfe66332e
Ludovic Barman <ludovic.barman@gmail.com>
2015-12-10 14:46:16 +0100

    [stash]

--- scripts-deterlab/config.sh
+++ scripts-deterlab/config.sh
@@ -5,3 +5,7 @@ socks="false"
 relayhostaddr="10.0.0.254:9876"
 logtype="netlogger"
 loghost="192.168.253.1:10000"
+
+nohupoutfolder="/tmp/"
+nohupclientname="client"
+nohupext=".nohup"
--- scripts-deterlab/localClientRun.sh
+++ scripts-deterlab/localClientRun.sh
@@ -1,7 +1,7 @@
 source config.sh
 
-echo "Killing processess..."
+echo "Killing processess named ${program}..."
 pkill -f ${program}
-echo "Starting client $1..."
-nohup ${programpath}${program} -client=$1 -socks=$socks -relayhostaddr=$relayhostaddr -logtype=$netlogger -loghost=$loghost &
+echo "Starting client $1, socks=$socks, relayhostaddr=$relayhostaddr, logtype=$netlogger -loghost=$loghost log redirected to ${nohupoutfolder}${nohupclientname}${nohupext}..."
+nohup ${programpath}${program} -client=$1 -socks=$socks -relayhostaddr=$relayhostaddr -logtype=$netlogger -loghost=$loghost 1>>${nohupoutfolder}${nohupclientname}${nohupext} 2>&1 &
 echo "Done."
