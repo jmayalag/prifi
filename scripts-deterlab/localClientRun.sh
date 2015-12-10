6ad9c6f1b372ac99dbe9130aaf400fdd08ccb047
Ludovic Barman <ludovic.barman@gmail.com>
2015-12-10 15:00:55 +0100

    [stash]

--- scripts-deterlab/localClientRun.sh
+++ scripts-deterlab/localClientRun.sh
@@ -1,7 +1,14 @@
 source ~/config.sh
 
+id=$1
+if [ $# -eq 0 ]
+  then
+    echo "First argument not given, ID=0"
+  id=0
+fi
+
 echo "Killing processess named ${program}..."
 pkill -f ${program}
-echo "Starting client $1, socks=$socks, relayhostaddr=$relayhostaddr, logtype=$netlogger -loghost=$loghost log redirected to ${nohupoutfolder}${nohupclientname}${nohupext}..."
-nohup "${programpath}${program}" -client=$1 -socks=$socks -relayhostaddr=$relayhostaddr -logtype=$netlogger -loghost=$loghost 1>>${nohupoutfolder}${nohupclientname}${nohupext} 2>&1 &
+echo "Starting client ${id}, cmd=nohup "${programpath}${program}" socks=$socks, relayhostaddr=$relayhostaddr, logtype=$netlogger -loghost=$loghost log redirected to ${nohupoutfolder}${nohupclientname}${id}${nohupext}..."
+nohup "${programpath}${program}" -client=$1 -socks=$socks -relayhostaddr=$relayhostaddr -logtype=$netlogger -loghost=$loghost 1>>${nohupoutfolder}${nohupclientname}${id}${nohupext} 2>&1 &
 echo "Done."
