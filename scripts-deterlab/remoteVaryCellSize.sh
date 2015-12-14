5efee6d3d1c700e4fbc546d638d94fa0afd23272
Ludovic Barman <ludovic.barman@gmail.com>
2015-12-14 17:09:44 +0100

    Lessen the pauses

--- scripts-deterlab/remoteVaryCellSize.sh
+++ scripts-deterlab/remoteVaryCellSize.sh
@@ -21,11 +21,11 @@ for repeat in $(seq 0 $nrepeat); do
 		echo "[$repeat/$nrepeat] Starting relay with $ntrustee trustees cellsize $cellSize"
 		ssh router.LB-LLD.SAFER.isi.deterlab.net "./dissent/localRelayRunNTrustee.sh $ntrustee $cellSize"
 		echo "[$repeat/$nrepeat] Waiting 60 sec for relay to setup..."
-		sleep 60
+		sleep 20
   
      	echo "[$repeat/$nrepeat] Starting client-0 cellsize $cellSize"
 		ssh client-0.LB-LLD.SAFER.isi.deterlab.net "./dissent/localClientRun.sh 0 $cellSize"
-		echo "[$repeat/$nrepeat] Waiting 30 sec before starting next client..."
+		echo "[$repeat/$nrepeat] Waiting 30 sec..."
 		sleep 30
 
 	done
