e14255bbe4052c87a0395d0c64a8e81b4c33eecf
Ludovic Barman <ludovic.barman@gmail.com>
2016-01-18 11:14:14 +0100

    New script

--- scripts-deterlab/remoteKillAll.sh
+++ scripts-deterlab/remoteKillAll.sh
@@ -6,17 +6,17 @@ source ~/config.sh
 maxtrustee=4
 maxclient=9
 
-echo "Remoting inside relay"
+echo "Remoting inside relay; pkill -f ${program}; rm -rf ${nohupoutfolder}${nohuprelayname}${nohupext}; rm -rf ${logPath}relay.out;"
 ssh router.LB-LLD.SAFER.isi.deterlab.net "pkill -f ${program}; rm -rf ${nohupoutfolder}${nohuprelayname}${nohupext}; rm -rf ${logPath}relay.out;"
 
 # Start clients
 for i in $(seq 0 $maxclient); do
-  echo "Remoting inside client-$i"
-  ssh client-$i.LB-LLD.SAFER.isi.deterlab.net "pkill -f ${program}; rm -rf ${nohupoutfolder}${nohupclientname}${id}${nohupext};rm -rf ${logPath}client${i}.out;"
+  echo "Remoting inside client-$i; pkill -f ${program}; rm -rf ${nohupoutfolder}${nohupclientname}${id}${nohupext}; rm -rf ${logPath}client${i}.out;"
+  ssh client-$i.LB-LLD.SAFER.isi.deterlab.net "pkill -f ${program}; rm -rf ${nohupoutfolder}${nohupclientname}${id}${nohupext}; rm -rf ${logPath}client${i}.out;"
 done
 
 # Start trustees
 for i in $(seq 0 $maxtrustee); do
-  echo "Remoting inside trustee-$i"
+  echo "Remoting inside trustee-$i; pkill -f ${program}; rm -rf ${nohupoutfolder}${nohuprelayname}${nohupext}; rm -rf ${logPath}trusteeServer.log;"
   ssh trustee-$i.LB-LLD.SAFER.isi.deterlab.net "pkill -f ${program}; rm -rf ${nohupoutfolder}${nohuprelayname}${nohupext}; rm -rf ${logPath}trusteeServer.log;"
 done
