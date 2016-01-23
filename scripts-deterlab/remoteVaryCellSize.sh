0a0256ab4f811d2b0dd2f783bcec5da130cb71f8
Ludovic Barman <ludovic.barman@gmail.com>
2016-01-23 10:56:12 +0100

    New script

--- scripts-deterlab/remoteVaryCellSize.sh
+++ scripts-deterlab/remoteVaryCellSize.sh
@@ -3,29 +3,31 @@
 #max trustee minus one, really
 nrepeat=9
 ntrustee=3
-maxcellsize=102400
+maxcellsize=61440
+downCellSize=40960
+window=1
 
 for repeat in $(seq 0 $nrepeat); do
 
 	echo "Repetition [$repeat/$nrepeat]"
 
-	for cellSize in $(seq 1024 1024 $maxcellsize); do
+	for upCellSize in $(seq 2048 2048 $maxcellsize); do
 
-		echo "[$repeat/$nrepeat] Killing everything..."
+		echo "[$repeat/$nrepeat][$downCellSize/$maxcellsize] Killing everything..."
 		/users/lbarman/dissent/remoteKillAll.sh
 
-		echo "[$repeat/$nrepeat] Starting the trustees..."
+		echo "[$repeat/$nrepeat][$downCellSize/$maxcellsize] Starting the trustees..."
 		/users/lbarman/dissent/remoteTrusteesSrvRun.sh
 		sleep 5
 
-		echo "[$repeat/$nrepeat] Starting relay with $ntrustee trustees cellsize $cellSize"
-		ssh router.LB-LLD.SAFER.isi.deterlab.net "./dissent/localRelayRunNTrustee.sh $ntrustee $cellSize"
-		echo "[$repeat/$nrepeat] Waiting 10 sec for relay to setup..."
+		echo "[$repeat/$nrepeat][$downCellSize/$maxcellsize] Starting relay with $ntrustee trustees, upCellSize $upCellSize downCellSize $downCellSize window $window"
+		ssh router.LB-LLD.SAFER.isi.deterlab.net "./dissent/localRelayRunNTrustee.sh $ntrustee  $upCellSize $downCellSize $window"
+		echo "[$repeat/$nrepeat][$downCellSize/$maxcellsize] Waiting 10 sec for relay to setup..."
 		sleep 10
   
-     	echo "[$repeat/$nrepeat] Starting client-0 cellsize $cellSize"
-		ssh client-0.LB-LLD.SAFER.isi.deterlab.net "./dissent/localClientRun.sh 0 $cellSize"
-		echo "[$repeat/$nrepeat] Waiting 30 sec..."
+     	echo "[$repeat/$nrepeat][$downCellSize/$maxcellsize] Starting client-0  upCellSize $upCellSize downCellSize $downCellSize"
+		ssh client-0.LB-LLD.SAFER.isi.deterlab.net "./dissent/localClientRun.sh 0 $upCellSize $downCellSize"
+		echo "[$repeat/$nrepeat][$downCellSize/$maxcellsize] Waiting 30 sec..."
 		sleep 30
 
 	done
