90fec29c7069dfd4cc602a5bdf36f7722edb13a3
moehajj <mohamad.elhajj95@hotmail.com>
2016-07-08 12:48:14 +0200

    Initializes but does not Connect to Web Server

--- SOCK5/client.go
+++ SOCK5/client.go
@@ -5,28 +5,27 @@ import (
 	"fmt"
 	"bufio"
 	"encoding/binary"
-	"strconv"
+	"io"
 )
 
 type dataWrap struct {
-	ID 		int
-	N 		int
+	ID 		uint32
+	N 		uint16
 	Data 	[] byte
 }
 
-func NewDataWrap(ID int, N int, Data []byte ) *dataWrap {
-    return &dataWrap{ID, N, Data}
+func NewDataWrap(ID uint32, N uint16, Data []byte ) dataWrap {
+    return dataWrap{ID, N, Data}
 }
 
 func (d *dataWrap) toBytes() []byte {
 	// Read up to a cell worth of data to send upstream
-	n := len(d.Data)
 	buffer := make([]byte, 6)
 	// Encode the connection number and actual data length
-	binary.BigEndian.PutUint32(buffer[0:4], uint32(d.ID))
-	binary.BigEndian.PutUint16(buffer[4:6], uint16(n))
+	binary.BigEndian.PutUint32(buffer[0:4], d.ID)
+	binary.BigEndian.PutUint16(buffer[4:6], d.N)
 
-	return append(buffer,d.Data...)
+	return append(buffer,d.Data[0:d.N]...)
 }
 
 func main() {
@@ -44,24 +43,59 @@ func main() {
 func connectToServer(IP string, toServer chan dataWrap, fromServer chan dataWrap) {
 	// connect to this socket
 	conn, _ := net.Dial("tcp", IP)
+	connReader := bufio.NewReader(conn)
 	fmt.Println("Connected To Server")
 
 	go func() {
 		for {
 			data := <-toServer
-    		fmt.Println("Sending Text ")
-    		conn.Write([]byte(string(data.Data) + "\n"))
+
+    		myBytes := data.toBytes()
+    		offset := 6
+
+    		fmt.Println("Connection",binary.BigEndian.Uint32(myBytes[0:4]),": Sending Message of length",data.N,"to Server")
+
+			if(data.N>3) {
+				fmt.Println("version", myBytes[offset+0])
+				fmt.Println("command", myBytes[offset+1])
+				fmt.Println("reserved", myBytes[offset+2])
+				fmt.Println("adderess type", myBytes[offset+3])
+				if int(myBytes[offset+3]) == 4 {
+					fmt.Println("IP adderess", net.IP(myBytes[offset+4:offset+20]).String())
+					fmt.Println("Port", binary.BigEndian.Uint16(myBytes[offset+20:offset+22]))
+				} else {
+					fmt.Println("IP adderess", net.IP(myBytes[offset+4:offset+8]).String())
+					fmt.Println("Port", binary.BigEndian.Uint16(myBytes[offset+8:offset+10]))
+				}
+
+				conn.Write(data.toBytes())
+			} else {
+				fmt.Println("version", myBytes[offset+0])
+				
+			}
+
+
+	   		conn.Write(data.toBytes())
+
 		}
 	}()
 
 	for {
-		newIDString, err1 := bufio.NewReader(conn).ReadString('\n')
-		message, err2 := bufio.NewReader(conn).ReadBytes('\n')
-		newID, _ := strconv.Atoi(newIDString)
+		newIDBytes := make([]byte, 4)
+    	_, errID := io.ReadFull(connReader,newIDBytes)
+   
+		fmt.Println("Received ConnID from server")	
+		message, errMessage := connReader.ReadBytes('\n')
+
+		fmt.Println("Received the data from server")	
+		newID := binary.BigEndian.Uint32( newIDBytes )
 		
-		if err1 == nil && err2 == nil {
-			fromServer <- *NewDataWrap( newID , len(message), message)
-   			fmt.Print("Message from server: "+ string(message))		
+		fmt.Println("Received reply from server for connection",newID)		
+
+
+		if errID == nil && errMessage == nil {
+			fromServer <- NewDataWrap( newID , uint16(len(message)), message)
+   			fmt.Println("Message from server: "+ string(message))		
 		}
 	}
 
@@ -90,26 +124,29 @@ func startSocksProxyServerListener(port string, newConnections chan<- net.Conn) 
 func startSocksProxyServerHandler(newConnections chan net.Conn, toServer chan dataWrap, fromServer chan dataWrap) {
 
 	socksProxyActiveConnections := make([]net.Conn, 1) // reserve socksProxyActiveConnections[0]
-	socksProxyConnClosed := make(chan int)
+	socksProxyConnClosed := make(chan uint32)
 
 	for {
 		select {
 
 		// New TCP connection to the SOCKS proxy
 		case conn := <-newConnections:
-			newSocksProxyId := len(socksProxyActiveConnections)
+			newSocksProxyId := uint32(len(socksProxyActiveConnections))
 			socksProxyActiveConnections = append(socksProxyActiveConnections, conn)
-			go readDataFromSocksProxy(newSocksProxyId, 1000, conn, toServer, socksProxyConnClosed)
+			go readDataFromSocksProxy(newSocksProxyId, 50, conn, toServer, socksProxyConnClosed)
+			fmt.Println("Connection", newSocksProxyId, "Established")
 
 		// Plaintext downstream data (relay->client->Socks proxy)
 		case myData := <-fromServer:
+			fmt.Println("We have a reply message to send to the broswer!")	
 			socksConnId := myData.ID
 			data := myData.Data
-			dataLength := myData.N
+			dataLength := len(data)
 
 			//Handle the connections, forwards the downstream slice to the SOCKS proxy
 			//if there is no socks proxy, nothing to do (useless case indeed, only for debug)
 			if dataLength > 0 && socksProxyActiveConnections[socksConnId] != nil {
+				fmt.Println("Sending Reply to Browser")	
 				socksProxyActiveConnections[socksConnId].Write(data)
 			} else {
 				socksProxyActiveConnections[socksConnId].Close()
@@ -123,21 +160,21 @@ func startSocksProxyServerHandler(newConnections chan net.Conn, toServer chan da
 	}
 }
 
-func readDataFromSocksProxy(socksConnId int, payloadLength int, conn net.Conn, toServer chan dataWrap, closed chan<- int) {
+func readDataFromSocksProxy(socksConnId uint32, payloadLength int, conn net.Conn, toServer chan dataWrap, closed chan<- uint32) {
 	for {
 		// Read up to a cell worth of data to send upstream
 		buffer := make([]byte, payloadLength)
-		n, _ := conn.Read(buffer[6:])
-
-		fmt.Println(string(buffer[7]))
-
-//		toServer <- *NewDataWrap(socksConnId,n,buffer)
+		n, _ := conn.Read(buffer)
 
 		// Connection error or EOF?
 		if n == 0 {
+			fmt.Println("Connection", socksConnId, "Closed")
 			conn.Close()
 			closed <- socksConnId // signal that channel is closed
 			return
 		}
+
+		toServer <- NewDataWrap(socksConnId,uint16(n),buffer)
+
 	}
 }
--- SOCK5/server.go
+++ SOCK5/server.go
@@ -1,9 +1,50 @@
 package main
 
-import "net"
-import "fmt"
-import "bufio"
-import "strings" // only needed below for sample processing
+import (
+  "net"
+  "fmt"
+  "bufio"
+  "io"
+  "encoding/binary"
+  "errors"
+  "strconv"
+)
+
+// Authentication methods
+const (
+  methNoAuth = iota
+  methGSS
+  methUserPass
+  methNone = 0xff
+)
+
+// Address types
+const (
+  addrIPv4   = 0x01
+  addrDomain = 0x03
+  addrIPv6   = 0x04
+)
+
+// Commands
+const (
+  cmdConnect   = 0x01
+  cmdBind      = 0x02
+  cmdAssociate = 0x03
+)
+
+// Reply codes
+const (
+  repSucceeded = iota
+  repGeneralFailure
+  repConnectionNotAllowed
+  repNetworkUnreachable
+  repHostUnreachable
+  repConnectionRefused
+  repTTLExpired
+  repCommandNotSupported
+  repAddressTypeNotSupported
+)
+
 
 func main() {
 
@@ -12,27 +53,353 @@ func main() {
   // listen on all interfaces
   ln, _ := net.Listen("tcp", ":8081")
 
-  // accept connection on port
-  conn, _ := ln.Accept()
-
-  // run loop forever (or until ctrl-c)
   for {
-    // will listen for message to process ending in newline (\n)
-    message, _ := bufio.NewReader(conn).ReadBytes('\n')
+    // accept connection on port
+    conn, _ := ln.Accept()
+
+   fmt.Println("Accepted Client Connection")   
+
+    go handleClient(conn)
+  }
+ 
+}
+
+func handleClient(conn net.Conn) {
     
-    client, _ := conn.RemoteAddr().(*net.TCPAddr)
-    fmt.Println(client.IP)
+    allChannels := make( map[uint32]chan []byte )
 
-    //sendMessage("127.0.0.1:6789", message)
+    fmt.Println("Handling Conection")
 
-    // output message received
-    fmt.Print("Message Received:", string(message))
-    // sample process for string received
-    newmessage := strings.ToUpper(string(message))
-    // send new string back to client
-    conn.Write([]byte(newmessage + "\n"))
+    /*   Client SOCKS Request Handling     */
+
+    connReader := bufio.NewReader(conn)
+
+    for {
+
+    connID, messageLength, err :=  readHeader(connReader)
+    if err != nil {
+      //handle error
+      fmt.Println("Header Error")
+      return
+    }
+
+    fmt.Println("Connection ID:", connID  )
+    fmt.Println("Length:", messageLength )
+
+
+    myChan := allChannels[connID]
+
+    if myChan == nil {
+
+      newChan := make(chan []byte)
+      allChannels[connID] = newChan
+
+      go hanndleChannel(conn, newChan, connID)
+
+      myChan = newChan
+
+    } 
+
+    clientPacket := make([]byte, messageLength)
+    _ , err = io.ReadFull(connReader,clientPacket)
+    if err != nil {
+      //handle error
+      fmt.Println("Data Read Error")
+      return
+    }
+
+    myChan <- clientPacket
+}
+    
+}
+
+
+
+func hanndleChannel(conn net.Conn, clientPacket chan []byte, connID uint32) {
+  
+  connReader := newChanReader(clientPacket)
+
+  //Read SOCKS Version
+    socksVersion := []byte{0}
+    _, err := io.ReadFull(connReader,socksVersion)
+    if err != nil {
+      //handle error
+      fmt.Println("Version Error")
+      return
+    } else if int(socksVersion[0]) != 5 {
+      //handle socks version
+      fmt.Println("Version:", int( socksVersion[0] ) )
+      return
+    }
+
+    fmt.Println( "Socks Version is:", int( socksVersion[0] ) )
+
+    //Read SOCKS Number of Methods
+    socksNumOfMethods := []byte{0}
+    _ , err = io.ReadFull(connReader,socksNumOfMethods)
+    if err != nil {
+      //handle error
+
+      return
+    }
+
+    fmt.Println("Socks Num of Methods is:", int(socksNumOfMethods[0] ) )
+
+    //Read SOCKS Methods
+    numOfMethods := int( socksNumOfMethods[0] )
+    socksMethods := make([]byte, numOfMethods)
+    _, err = io.ReadFull(connReader,socksMethods)
+    if err != nil {
+      //handle error
+
+      return
+    }
+
+
+
+    // Find a supported method (currently only NoAuth)
+    foundMethod := false
+    for i := 0; i< len(socksMethods); i++ {
+      if socksMethods[i] == methNoAuth {
+        foundMethod = true
+        break
+      }
+    }
+
+    if !foundMethod {
+      //handle not finding method
+
+      return
+    }
+
+    fmt.Println("Constructing Response")
+
+    //Construct Response Message
+    methodSelectionResponse := []byte{ socksVersion[0] , byte(methNoAuth) }
+    replyToClient(conn, connID, methodSelectionResponse)
+
+    fmt.Println("Sent Response")
+
+
+    /*   Client Web Request Handling    */
+
+    requestHeader := make([] byte, 4)
+    _, err = io.ReadFull(connReader,requestHeader)
+    if err != nil {
+      //handle error
+      fmt.Println("Request Header Error")
+      return
+    }
+
+    fmt.Println("Version Type:", int( requestHeader[0] ) )
+    fmt.Println("CMD Type:", int( requestHeader[1] ) )
+    fmt.Println("Reserved Type:", int( requestHeader[2] ) )
+    fmt.Println("Address Type:", int( requestHeader[3] ) )
+
+    destinationIP, err :=  readSocksAddr(connReader, int(requestHeader[3]))
+    if err != nil {
+      //handle error
+      fmt.Println("IP Address Error")
+      return
+    }
+
+
+    destinationPortBytes := make([]byte, 2)
+    _, err = io.ReadFull(connReader,destinationPortBytes)
+    if err != nil {
+      //handle error
+      fmt.Println("Destination Port Error")
+      return
+    }
+    destinationPort := binary.BigEndian.Uint16(destinationPortBytes)
+
+
+    destinationAddress := (&net.TCPAddr{IP: destinationIP, Port: int(destinationPort)}).String()
+    //destinationAddress := fmt.Sprintf("%s:%d", destinationIP, destinationPort)
+
+    fmt.Println("Web server @",destinationAddress)
+
+    // Process the command
+    switch int(requestHeader[1]) {
+      case cmdConnect:
+        webConn, err := net.Dial("tcp", destinationAddress)
+        if err != nil {
+          fmt.Println("Failed to connect to web server")
+          return
+        }
+
+        // Send success reply downstream
+        sucessMessage := createSocksReply(repSucceeded, conn.LocalAddr())
+        replyToClient(conn, connID, sucessMessage)
+
+        // Commence forwarding raw data on the connection
+        go socksRelayDown(webConn, conn, connID)
+        go socksRelayUp(webConn, connReader, connID)
+
+      default:
+        fmt.Println("Cannot Process Command")
+    }
+
+}
+
+
+
+
+func socksRelayDown(webConn net.Conn, conn net.Conn, connID uint32) {
+  for {
+    buf := make([]byte, 100000)
+    n, _ := webConn.Read(buf)
+    buf = buf[:n]
+    // Forward the data (or close indication if n==0) downstream
+    replyToClient(conn, connID, buf)
+
+    // Connection error or EOF?
+    if n == 0 {
+      webConn.Close()
+      return
+    }
   }
 }
+
+
+func socksRelayUp(webConn net.Conn, connReader io.Reader, connID uint32) {
+
+  for {
+    // Get the next upstream data buffer
+    buf := make([]byte, 100000)
+    messageLength, err := io.ReadFull(connReader,buf)
+    if err != nil {
+      //handle error
+      fmt.Println("Header Error")
+      return
+    }
+
+    if messageLength == 0 { // connection close indicator
+      return
+    }
+    //println(hex.Dump(buf))
+    n, err := webConn.Write(buf[:messageLength])
+    if n != messageLength {
+      return
+    }
+
+  }
+}
+
+
+
+
+
+
+func replyToClient(conn net.Conn, connID uint32, message []byte) {
+  byteID := make([]byte,4)
+  binary.BigEndian.PutUint32(byteID, connID)
+
+  conn.Write(byteID)
+  conn.Write([]byte( string(message) + "\n"))
+
+  fmt.Println("Replied to Client with ID:", connID)
+}
+
+// Read an IPv4 or IPv6 address from an io.Reader and return it as a string
+func readIP(r io.Reader, len int) (net.IP, error) {
+  errorIP := make(net.IP, net.IPv4len)
+
+  addr := make([]byte, len)
+  _, err := io.ReadFull(r, addr)
+  if err != nil {
+    return errorIP, err
+  }
+  return net.IP(addr), nil
+}
+
+func readSocksAddr(cr io.Reader, addrtype int) (net.IP, error) {
+  
+ errorIP := make(net.IP, net.IPv4len)
+
+  switch addrtype {
+  case addrIPv4:
+    return readIP(cr, net.IPv4len)
+
+  case addrIPv6:
+    return readIP(cr, net.IPv6len)
+
+  case addrDomain:
+
+    // First read the 1-byte domain name length
+    dlen := [1]byte{}
+    _, err := io.ReadFull(cr, dlen[:])
+    if err != nil {
+      return errorIP, err
+    }
+
+    // Now the domain name itself
+    domain := make([]byte, int(dlen[0]))
+    _, err = io.ReadFull(cr, domain)
+    if err != nil {
+      return errorIP, err
+    }
+
+    return net.IP(domain), nil
+
+  default:
+    msg := fmt.Sprintf("unknown SOCKS address type %d", addrtype)
+    fmt.Println(msg)
+    return errorIP, errors.New(msg)
+  }
+
+}
+
+
+func createSocksReply(replyCode int, addr net.Addr) []byte {
+  
+  buf := make([]byte, 4)
+  buf[0] = byte(5) // version
+  buf[1] = byte(replyCode)
+
+   // Address type
+  if addr != nil {
+
+    tcpaddr := addr.(*net.TCPAddr)
+    host4 := tcpaddr.IP.To4()
+    host6 := tcpaddr.IP.To16()
+
+    i, _ := strconv.Atoi("6789")
+
+    fmt.Println("Success Address",tcpaddr.IP)
+    fmt.Println("Success Port",i)
+
+    port := [2]byte{}
+    binary.BigEndian.PutUint16(port[:], uint16(i))//tcpaddr.Port))
+
+    if host4 != nil { // it's an IPv4 address
+
+      buf[3] = addrIPv4
+      buf = append(buf, host4...)
+      buf = append(buf, port[:]...)
+
+    } else if host6 != nil { // it's an IPv6 address
+
+      buf[3] = addrIPv6
+      buf = append(buf, host6...)
+      buf = append(buf, port[:]...)
+
+    } else { // huh???
+
+      fmt.Println("SOCKS: neither IPv4 nor IPv6 addr?")
+      addr = nil
+      buf[1] = byte(repAddressTypeNotSupported)
+
+    }
+
+  } else { // attach a null IPv4 address
+    buf[3] = addrIPv4
+    buf = append(buf, make([]byte, 4+2)...)
+  }
+
+   return buf
+ }
+
 
 
 func sendMessage(IP string, message []byte) {
@@ -46,4 +413,57 @@ func sendMessage(IP string, message []byte) {
 
   conn.Close()
 
+}
+
+func readHeader(connReader io.Reader) (uint32, uint16, error) {
+  
+  controlHeader := make([]byte, 6)
+
+  _, err := io.ReadFull(connReader,controlHeader)  
+  if err != nil {
+    return 0, 0, err
+  }
+
+  connID := binary.BigEndian.Uint32(controlHeader[0:4])
+  messageLength := binary.BigEndian.Uint16(controlHeader[4:6])
+
+  return connID, messageLength, nil
+
+}
+
+type chanreader struct {
+  b   []byte
+  c   <-chan []byte
+  eof bool
+}
+
+func (cr *chanreader) Read(p []byte) (n int, err error) {
+  if cr.eof {
+    return 0, io.EOF
+  }
+  blen := len(cr.b)
+  if blen == 0 {
+    cr.b = <-cr.c // read next block from channel
+    blen = len(cr.b)
+    if blen == 0 { // channel sender signaled EOF
+      cr.eof = true
+      return 0, io.EOF
+    }
+  }
+
+  act := min(blen, len(p))
+  copy(p, cr.b[:act])
+  cr.b = cr.b[act:]
+  return act, nil
+}
+
+func newChanReader(c <-chan []byte) *chanreader {
+  return &chanreader{[]byte{}, c, false}
+}
+
+func min(x, y int) int {
+  if x < y {
+    return x
+  }
+  return y
 }
