dc38f7a5a1dae46111c312ac73b3b8f7bc844062
Ludovic Barman <ludovic.barman@gmail.com>
2016-12-14 17:30:28 +0100

    Browsing through PriFi works ! yeaaaaah

--- prifi-lib/relay.go
+++ prifi-lib/relay.go
@@ -218,8 +218,6 @@ func NewRelayState(nTrustees int, nClients int, upstreamCellSize int, downstream
 	// Sets the new state
 	params.currentState = RELAY_STATE_COLLECTING_TRUSTEES_PKS
 
-	go socks.StartSocksClient("127.0.0.1:8081", params.DataFromDCNet, params.PriorityDataForClients)
-
 	return params
 }
 
