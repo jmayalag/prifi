05b1b1473b8aa81223cacee4fcddbe71e335bbc1
Ludovic Barman <ludovic.barman@gmail.com>
2017-01-08 20:38:45 +0100

    100% coverage for config

--- prifi-lib/config/config.go
+++ prifi-lib/config/config.go
@@ -4,12 +4,18 @@ Package config contains the cryptographic primitives that are used by the PriFi 
 package config
 
 import (
+	"github.com/dedis/crypto/abstract"
 	"github.com/dedis/crypto/ed25519"
 	"github.com/lbarman/prifi/prifi-lib/dcnet"
 )
 
 //CryptoSuite contains crypto suite used, here ED25519
-var CryptoSuite = ed25519.NewAES128SHA256Ed25519(false) //nist.NewAES128SHA256P256()
+var CryptoSuite abstract.Suite
 
 //Factory contains the factory for the DC-net's cell encoder/decoder.
-var Factory = dcnet.SimpleCoderFactory
+var Factory func() dcnet.CellCoder
+
+func init() {
+	CryptoSuite = ed25519.NewAES128SHA256Ed25519(false) //nist.NewAES128SHA256P256()
+	Factory = dcnet.SimpleCoderFactory
+}
