1b55c68a70c68e0f419516b864d1899526b03ff7
Ludovic Barman <ludovic.barman@gmail.com>
2017-01-09 00:03:25 +0100

    BugFix in prifi.sh

--- prifi.sh
+++ prifi.sh
@@ -161,7 +161,7 @@ case $1 in
 		echo -e "$okMsg"
 
 		echo -n "Re-getting all go packages (since we switched branch)... "
-		cd sda/app; go get ./... 1>/dev/null 2>&1
+		cd $GOPATH/src/github.com/lbarman/prifi/sda/app; go get ./... 1>/dev/null 2>&1
 		cd ../..
 		cd $GOPATH/src/github.com/dedis/cothority; go get ./... 1>/dev/null 2>&1
 		echo -e "$okMsg"
