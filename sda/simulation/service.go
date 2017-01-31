c11e225740c612eb2211ac17769054ee4d0bb151
Ludovic Barman <ludovic.barman@gmail.com>
2017-01-31 14:03:06 +0100

    Needs a app.Group

--- sda/simulation/service.go
+++ sda/simulation/service.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"github.com/BurntSushi/toml"
-	"github.com/lbarman/prifi/sda/services"
+	prifi_service "github.com/lbarman/prifi/sda/services"
+	prifi_protocol "github.com/lbarman/prifi/sda/protocols"
 	"gopkg.in/dedis/onet.v1"
 	"gopkg.in/dedis/onet.v1/log"
 	"gopkg.in/dedis/onet.v1/simul/monitor"
 	"time"
+	"github.com/dedis/cothority/services"
+	"os"
+	"io/ioutil"
 )
 
 /*
@@ -46,6 +50,8 @@ func (s *SimulationService) Setup(dir string, hosts []string) (*onet.SimulationC
 	return sc, nil
 }
 
+var prifiService *prifi_service.ServiceState
+
 // Node can be used to initialize each node before it will be run
 // by the server. Here we call the 'Node'-method of the
 // SimulationBFTree structure which will load the roster- and the
@@ -56,7 +62,35 @@ func (s *SimulationService) Node(config *onet.SimulationConfig) error {
 		log.Fatal("Didn't find this node in roster")
 	}
 	log.Lvl3("Initializing node-index", index)
-	return s.SimulationBFTree.Node(config)
+	if err := s.SimulationBFTree.Node(config); err != nil {
+		log.Fatal("Could not register node in SDA Tree", err)
+	}
+
+	//read the config
+	prifiTomlConfig, err := readPriFiConfigFile("filepath.txt")
+	if err != nil {
+		log.Fatal("Could not read PriFi config", err)
+	}
+
+	//finds the PriFi service
+	service := config.GetService(prifi_service.ServiceName).(*prifi_service.ServiceState)
+
+	//set the config from the .toml file
+	service.SetConfigFromToml(prifiTomlConfig)
+
+
+	switch index{
+	case 0:
+		log.Lvl1("Intiating this node as relay")
+		prifiService = service.StartRelay( /* need app.Group here */ )
+
+	}
+
+	/*
+	host.Router.AddErrorHandler(service.NetworkErrorHappened)
+	host.Start()
+	 */
+	return nil
 }
 
 // Run is used on the destination machines and runs a number of
@@ -73,11 +107,11 @@ func (s *SimulationService) Run(config *onet.SimulationConfig) error {
 		round := monitor.NewTimeMeasure("round")
 
 		/*
-		ret, err := service.ClockRequest(&template.ClockRequest{Roster: config.Roster})
-		if err != nil {
-			log.Error(err)
-		}
-		resp, ok := ret.(*template.ClockResponse)$
+			ret, err := service.ClockRequest(&template.ClockRequest{Roster: config.Roster})
+			if err != nil {
+				log.Error(err)
+			}
+			resp, ok := ret.(*template.ClockResponse)$
 		*/
 		time.Sleep(time.Second)
 
@@ -87,4 +121,27 @@ func (s *SimulationService) Run(config *onet.SimulationConfig) error {
 		round.Record()
 	}
 	return nil
+}
+
+
+func readPriFiConfigFile(filePath string) (*prifi_protocol.PrifiTomlConfig, error) {
+
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		log.Error("Could not open file \"", filePath, "\" (specified by flag prifi_config)")
+		return nil, err
+	}
+
+	tomlRawData, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Error("Could not read file \"", filePath, "\" (specified by flag prifi_config)")
+	}
+
+	tomlConfig := &prifi_protocol.PrifiTomlConfig{}
+	_, err = toml.Decode(string(tomlRawData), tomlConfig)
+	if err != nil {
+		log.Error("Could not parse toml file", filePath)
+		return nil, err
+	}
+
+	return tomlConfig, nil
 }
