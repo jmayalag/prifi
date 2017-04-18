9c245984604b02f83aed3082846535ddd4d7f119
Ludovic Barman <ludovic.barman@gmail.com>
2017-04-18 15:00:37 +0200

    Fix prifi-lib/net test

--- prifi-lib/net/net_test.go
+++ prifi-lib/net/net_test.go
@@ -25,7 +25,7 @@ func (t *TestMessageSender) SendToRelay(msg interface{}) error {
 func (t *TestMessageSender) BroadcastToAllClients(msg interface{}) error {
 	return nil
 }
-func (t *TestMessageSender) ClientSubscribeToBroadcast(clientName string, messageReceived func(interface{}) error, startStopChan chan bool) error {
+func (t *TestMessageSender) ClientSubscribeToBroadcast(clientID int, messageReceived func(interface{}) error, startStopChan chan bool) error {
 	return nil
 }
 
