cf7a0b4307ebbd8a334abf8343ad14e93f1a4641
PierreSarton <pierre.sarton@epfl.ch>
2017-06-09 15:25:42 +0200

    Trustee tests.

--- prifi-lib/trustee/trustee_test.go
+++ prifi-lib/trustee/trustee_test.go
@@ -45,7 +45,7 @@ func (t *TestMessageSender) ClientSubscribeToBroadcast(clientID int, messageRece
 
 func newTestMessageSenderWrapper(msgSender net.MessageSender) *net.MessageSenderWrapper {
 
-	sentToRelay = make(chan interface{}, 10)
+	sentToRelay = make(chan interface{}, 15)
 	errHandling := func(e error) {}
 	loggingSuccessFunction := func(e interface{}) { log.Lvl3(e) }
 	loggingErrorFunction := func(e interface{}) { log.Error(e) }
@@ -324,4 +324,274 @@ func TestTrustee(t *testing.T) {
 		t.Error("Trustee should be in state SHUTDOWN")
 	}
 
+}
+
+func TestTrusteeBlame(t *testing.T) {
+
+	msgSender := new(TestMessageSender)
+	msw := newTestMessageSenderWrapper(msgSender)
+	trustee := NewTrustee(msw)
+
+	ts := trustee.trusteeState
+	if ts.sendingRate == nil {
+		t.Error("sendingRate should not be nil")
+	}
+	if trustee.stateMachine.State() != "BEFORE_INIT" {
+		t.Error("State was not set correctly")
+	}
+	if ts.privateKey == nil || ts.PublicKey == nil {
+		t.Error("Private/Public key not set")
+	}
+	if ts.neffShuffle == nil {
+		t.Error("NeffShuffle should not be nil")
+	}
+
+	//should not be able to receive those weird messages
+	weird := new(net.ALL_ALL_PARAMETERS_NEW)
+	weird.Add("NextFreeTrusteeID", -1)
+	if err := trustee.ReceivedMessage(*weird); err == nil {
+		t.Error("Trustee should not accept this message")
+	}
+	weird.Add("NextFreeTrusteeID", 0)
+	weird.Add("NTrustees", 0)
+	if err := trustee.ReceivedMessage(*weird); err == nil {
+		t.Error("Trustee should not accept this message")
+	}
+	weird.Add("NTrustees", 1)
+	weird.Add("NClients", 0)
+	if err := trustee.ReceivedMessage(*weird); err == nil {
+		t.Error("Trustee should not accept this message")
+	}
+	weird.Add("NClients", 1)
+	weird.Add("UpstreamCellSize", 0)
+	if err := trustee.ReceivedMessage(*weird); err == nil {
+		t.Error("Trustee should not accept this message")
+	}
+
+	//we start by receiving a ALL_ALL_PARAMETERS from relay
+	msg := new(net.ALL_ALL_PARAMETERS_NEW)
+	msg.ForceParams = true
+	trusteeID := 3
+	nClients := 3
+	nTrustees := 2
+	upCellSize := 1500
+	dcNetType := "Simple"
+	msg.Add("StartNow", true)
+	msg.Add("NClients", nClients)
+	msg.Add("NTrustees", nTrustees)
+	msg.Add("UpstreamCellSize", upCellSize)
+	msg.Add("NextFreeTrusteeID", trusteeID)
+	msg.Add("DCNetType", dcNetType)
+
+	if err := trustee.ReceivedMessage(*msg); err != nil {
+		t.Error("Trustee should be able to receive this message:", err)
+	}
+
+	if ts.nClients != 3 {
+		t.Error("NClients should be 3")
+	}
+	if ts.nTrustees != nTrustees {
+		t.Error("nTrustees should be 2")
+	}
+	if ts.PayloadLength != 1500 {
+		t.Error("PayloadLength should be 1500")
+	}
+	if ts.ID != trusteeID {
+		t.Error("ID should be 3")
+	}
+	if ts.DCNet_RoundManager == nil {
+		t.Error("DCNet_RoundManager should have been created")
+	}
+	if len(ts.ClientPublicKeys) != nClients {
+		t.Error("Len(TrusteePKs) should be equal to NTrustees")
+	}
+	if len(ts.sharedSecrets) != nClients {
+		t.Error("Len(SharedSecrets) should be equal to NTrustees")
+	}
+	if trustee.stateMachine.State() != "INITIALIZING" {
+		t.Error("Trustee should be in state INITIALIZING")
+	}
+
+	//Should send a TRU_REL_TELL_PK
+	select {
+	case msg3 := <-sentToRelay:
+		msg3_parsed := msg3.(*net.TRU_REL_TELL_PK)
+		if msg3_parsed.TrusteeID != trusteeID {
+			t.Error("Trustee sent a wrong trustee ID")
+		}
+		if !msg3_parsed.Pk.Equal(ts.PublicKey) {
+			t.Error("Trustee did not send his public key")
+		}
+	default:
+		t.Error("Trustee should have sent a TRU_REL_TELL_PK to the relay")
+	}
+
+	//do the shuffle
+	n := new(scheduler.NeffShuffle)
+	n.Init()
+	n.RelayView.Init(1)
+
+	clientPubKeys := make([]abstract.Point, nClients)
+	clientPrivKeys := make([]abstract.Scalar, nClients)
+	for i := 0; i < nClients; i++ {
+		clientPubKeys[i], clientPrivKeys[i] = crypto.NewKeyPair()
+		n.RelayView.AddClient(clientPubKeys[i])
+	}
+	toSend, _, err := n.RelayView.SendToNextTrustee()
+	if err != nil {
+		t.Error(err)
+	}
+	msg4 := toSend.(*net.REL_TRU_TELL_CLIENTS_PKS_AND_EPH_PKS_AND_BASE)
+
+	//we inject the public keys
+	msg4.Pks = make([]abstract.Point, nClients)
+	for i := 0; i < nClients; i++ {
+		msg4.Pks[i] = clientPubKeys[i]
+	}
+
+	//we receive the shuffle
+	if err := trustee.ReceivedMessage(*msg4); err != nil {
+		t.Error("Trustee should be able to receive this message:", err)
+	}
+
+	for i := 0; i < nClients; i++ {
+		if !ts.ClientPublicKeys[i].Equal(clientPubKeys[i]) {
+			t.Error("Pub key", i, "has not been stored correctly")
+		}
+		myPrivKey := ts.privateKey
+		if !ts.sharedSecrets[i].Equal(config.CryptoSuite.Point().Mul(clientPubKeys[i], myPrivKey)) {
+			t.Error("Shared secret", i, "has not been computed correctly")
+		}
+	}
+
+	if trustee.stateMachine.State() != "SHUFFLE_DONE" {
+		t.Error("Trustee should be in state SHUFFLE DONE")
+	}
+
+	//Should have sent a TRU_REL_TELL_NEW_BASE_AND_EPH_PKS
+	select {
+	case msg5 := <-sentToRelay:
+		msg5_parsed := msg5.(*net.TRU_REL_TELL_NEW_BASE_AND_EPH_PKS)
+		_, err = n.RelayView.ReceivedShuffleFromTrustee(msg5_parsed.NewBase, msg5_parsed.NewEphPks, msg5_parsed.Proof)
+		if err != nil {
+			t.Error("This should be fine, yet", err)
+		}
+	default:
+		t.Error("Trustee should have sent a TRU_REL_TELL_NEW_BASE_AND_EPH_PKS to the relay")
+	}
+
+	//should receive the transcript
+	toSend3, err := n.RelayView.SendTranscript()
+	if err != nil {
+		t.Error(err)
+	}
+	msg6 := toSend3.(*net.REL_TRU_TELL_TRANSCRIPT)
+
+	if err := trustee.ReceivedMessage(*msg6); err != nil {
+		t.Error("Trustee should be able to receive this message:", err)
+	}
+
+	//should the signed shuffle
+	select {
+	case msgX := <-sentToRelay:
+		_ = msgX.(*net.TRU_REL_SHUFFLE_SIG)
+	default:
+		t.Error("Trustee should have sent a TRU_REL_SHUFFLE_SIG to the relay")
+	}
+
+	if trustee.stateMachine.State() != "READY" {
+		t.Error("Trustee should be in state READY")
+	}
+
+	stopMsg := &net.REL_TRU_TELL_RATE_CHANGE{
+		WindowCapacity: 0,
+	}
+
+	time.Sleep(TRUSTEE_BASE_SLEEP_TIME / 2) //just time for one message
+
+	if err := trustee.ReceivedMessage(*stopMsg); err != nil {
+		t.Error("Should handle this stop message, but", err)
+	}
+
+	//should have sent a few ciphers before getting the stop message
+	select {
+	case msg8 := <-sentToRelay:
+		msg8_parsed := msg8.(*net.TRU_REL_DC_CIPHER)
+
+		if msg8_parsed.TrusteeID != trusteeID {
+			t.Error("TRU_REL_DC_CIPHER has the wrong trustee ID")
+		}
+		if msg8_parsed.RoundID != 0 {
+			t.Error("TRU_REL_DC_CIPHER has the wrong round ID")
+		}
+		if len(msg8_parsed.Data) != upCellSize {
+			t.Error("TRU_REL_DC_CIPHER sent a payload with wrong size")
+		}
+
+	default:
+		t.Error("Trustee should have sent a TRU_REL_DC_CIPHER to the relay")
+	}
+
+	time.Sleep(TRUSTEE_BASE_SLEEP_TIME * 2)
+
+	//empty the chan
+	empty := false
+	for !empty {
+		select {
+		case <-sentToRelay:
+			//nothing
+		default:
+			empty = true
+		}
+	}
+
+	msg7 := &net.REL_ALL_REVEAL{
+		RoundID: 1,
+		BitPos:  1}
+	if err := trustee.ReceivedMessage(*msg7); err != nil {
+		t.Error("Trustee should be able to receive this message:", err)
+	}
+
+	select {
+	case msg9 := <-sentToRelay:
+		msg9_parsed := msg9.(*net.TRU_REL_REVEAL)
+
+		if msg9_parsed.TrusteeID != trusteeID {
+			t.Error("TRU_REL_REVEAL has the wrong trustee ID")
+		}
+		if msg9_parsed.Bits == nil {
+			t.Error("TRU_REL_REVEAL did not send bits")
+		}
+		if len(msg9_parsed.Bits) != nClients {
+			t.Error("TRU_REL_REVEAL did not send 1 bit per client")
+		}
+
+	default:
+		t.Error("Trustee should have sent a TRU_REL_REVEAL to the relay")
+	}
+
+	if trustee.stateMachine.State() != "BLAMING" {
+		t.Error("Trustee should be in state BLAMING")
+	}
+
+	msg10 := &net.REL_ALL_SECRET{
+		UserID: 1}
+	if err := trustee.ReceivedMessage(*msg10); err != nil {
+		t.Error("Trustee should be able to receive this message:", err)
+	}
+
+	select {
+	case msg11 := <-sentToRelay:
+		msg11_parsed := msg11.(*net.TRU_REL_SECRET)
+
+		if msg11_parsed.Secret.Equal(config.CryptoSuite.Point().Mul(ts.PublicKey, clientPrivKeys[0])) {
+			t.Error("Trustee did not send his secret")
+		}
+		if msg11_parsed.NIZK == nil {
+			t.Error("TRU_REL_REVEAL did not contain the NIZK")
+		}
+
+	default:
+		t.Error("Trustee should have sent a TRU_REL_REVEAL to the relay")
+	}
 }
