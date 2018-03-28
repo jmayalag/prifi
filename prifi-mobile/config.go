5d73bd915ca87054709442a87d3f77b7460c9c89
Jun Chen <jun.chen@epfl.ch>
2018-03-28 17:41:32 +0200

    Simplify the implementation

--- prifi-mobile/config.go
+++ prifi-mobile/config.go
@@ -9,114 +9,67 @@ import (
 	"golang.org/x/mobile/asset"
 	"gopkg.in/dedis/onet.v1/app"
 	"gopkg.in/dedis/onet.v1/log"
-	"gopkg.in/dedis/onet.v1/network"
 	"sync"
 )
 
-const mobileClientConfigFilename = "prifi.toml"
+const prifiConfigFilename = "prifi.toml"
 const cothorityConfigFilename = "identity.toml"
 const cothorityGroupConfigFilename = "group.toml"
 
-var prifiMobileClientConfigSingleton *PrifiMobileClientConfig
-var cothorityConfigSingleton *CothorityConfig
+var prifiConfigSingleton *prifi_protocol.PrifiTomlConfig
+var cothorityConfigSingleton *app.CothorityConfig
 
 var onceClient, onceCothority sync.Once
+var globalErr error
 
-// Exposed singleton constructors (Functions with New... will become constructors that can be called by Java and ObjC)
-func NewPrifiMobileClientConfig() *PrifiMobileClientConfig {
+
+func getPrifiConfig() (*prifi_protocol.PrifiTomlConfig, error) {
 	onceClient.Do(func() {
-		prifiMobileClientConfigSingleton = initPrifiMobileClientConfig()
+		prifiConfigSingleton, globalErr = initPrifiConfig()
 	})
-	return prifiMobileClientConfigSingleton
+	return prifiConfigSingleton, globalErr
 }
 
-func NewCothorityConfig() *CothorityConfig {
+func getCothorityConfig() (*app.CothorityConfig, error) {
 	onceCothority.Do(func() {
-		cothorityConfigSingleton = initCothorityConfig()
+		cothorityConfigSingleton, globalErr = initCothorityConfig()
 	})
-	return cothorityConfigSingleton
-}
-
-// Exposed structs and their getters and setters
-// Design Choice: PrifiConfig and Cothority Config can be manipulated by Mobile OS, that's why they are exposed
-type PrifiMobileClientConfig struct {
-	ForceConsoleColor                       bool
-	OverrideLogLevel                        int
-	ClientDataOutputEnabled                 bool
-	RelayDataOutputEnabled                  bool
-	CellSizeUp                              int
-	CellSizeDown                            int
-	RelayWindowSize                         int
-	RelayUseOpenClosedSlots                 bool
-	RelayUseDummyDataDown                   bool
-	RelayReportingLimit                     int
-	UseUDP                                  bool
-	DoLatencyTests                          bool
-	SocksServerPort                         int
-	SocksClientPort                         int
-	ProtocolVersion                         string
-	DCNetType                               string
-	ReplayPCAP                              bool
-	PCAPFolder                              string
-	TrusteeSleepTimeBetweenMessages         int
-	TrusteeAlwaysSlowDown                   bool
-	TrusteeNeverSlowDown                    bool
-	SimulDelayBetweenClients                int
-	DisruptionProtectionEnabled             bool
-	EquivocationProtectionEnabled           bool
-	OpenClosedSlotsMinDelayBetweenRequests  int
-	RelayMaxNumberOfConsecutiveFailedRounds int
-	RelayProcessingLoopSleepTime            int
-	RelayRoundTimeOut                       int
-	RelayTrusteeCacheLowBound               int
-	RelayTrusteeCacheHighBound              int
-}
-
-// network.Address a type that is currently not supported by gomobile (23 March 2018)
-// Thus, we have a function to return address under a supported type (string)
-type CothorityConfig struct {
-	Public      string
-	Private     string
-	Address     network.Address
-	Description string
-}
-
-func (c *CothorityConfig) GetAddress() string {
-	return c.Address.String()
-}
-
-// Convert mobile config into original prifi config
-// TODO: Hanlde more carefully. What if mobile config has less or more members
-func (c *PrifiMobileClientConfig) parseToOriginalPrifiConfig() *prifi_protocol.PrifiTomlConfig {
-	config := prifi_protocol.PrifiTomlConfig(*c)
-	return &config
+	return cothorityConfigSingleton, globalErr
 }
 
 // TODO: Reduce Code Duplication of both inits
-func initPrifiMobileClientConfig() *PrifiMobileClientConfig {
-	tomlRawDataString := readTomlFromAssets(mobileClientConfigFilename)
+func initPrifiConfig() (*prifi_protocol.PrifiTomlConfig, error) {
+	tomlRawDataString, err := readTomlFromAssets(prifiConfigFilename)
 
-	config := &PrifiMobileClientConfig{}
-	_, err := toml.Decode(tomlRawDataString, config)
 	if err != nil {
-		log.Error("Could not parse toml file ", mobileClientConfigFilename)
-		return nil
+		return nil, err
 	}
 
-	return config
+	config := &prifi_protocol.PrifiTomlConfig{}
+	_, err = toml.Decode(tomlRawDataString, config)
+	if err != nil {
+		log.Error("Could not parse toml file ", prifiConfigFilename)
+		return nil, err
+	}
+
+	return config, nil
 }
 
-func initCothorityConfig() *CothorityConfig {
-	tomlRawDataString := readTomlFromAssets(cothorityConfigFilename)
+func initCothorityConfig() (*app.CothorityConfig, error) {
+	tomlRawDataString, err := readTomlFromAssets(cothorityConfigFilename)
 
-	config := &CothorityConfig{}
-	_, err := toml.Decode(tomlRawDataString, config)
 	if err != nil {
-		log.Error("Could not parse toml file ", cothorityConfigFilename)
-		return nil
+		return nil, err
 	}
 
-	return config
+	config := &app.CothorityConfig{}
+	_, err = toml.Decode(tomlRawDataString, config)
+	if err != nil {
+		log.Error("Could not parse toml file ", cothorityConfigFilename)
+		return nil, err
+	}
+
+	return config, nil
 }
 
 // TODO: less code duplication?
@@ -139,23 +92,23 @@ func readCothorityGroupConfig() (*app.Group, error) {
 	return groups, nil
 }
 
-// TODO: Read form any given paths
-func readTomlFromAssets(filename string) string {
+// TODO: Read from any given paths
+func readTomlFromAssets(filename string) (string, error) {
 	file, err := asset.Open(filename)
 	defer file.Close()
 
 	if err != nil {
 		log.Error("Could not open file ", filename)
-		return ""
+		return "", err
 	}
 
 	tomlRawDataBuffer := new(bytes.Buffer)
 	_, err = tomlRawDataBuffer.ReadFrom(file)
 
 	if err != nil {
-		log.Error("Could not read file ", mobileClientConfigFilename)
-		return ""
+		log.Error("Could not read file ", prifiConfigFilename)
+		return "", err
 	}
 
-	return tomlRawDataBuffer.String()
+	return tomlRawDataBuffer.String(), err
 }
---
+++ prifi-mobile/prepare.go
@@ -0,0 +1,74 @@
+package prifiMobile
+
+// Functions that are needed to initialize a server are all here
+
+import (
+	prifi_service "github.com/lbarman/prifi/sda/services"
+	prifi_protocol "github.com/lbarman/prifi/sda/protocols"
+	"gopkg.in/dedis/onet.v1"
+	"gopkg.in/dedis/onet.v1/app"
+	"gopkg.in/dedis/onet.v1/crypto"
+	"gopkg.in/dedis/onet.v1/log"
+	"gopkg.in/dedis/onet.v1/network"
+)
+
+func startCothorityNode() (*onet.Server, *app.Group, *prifi_service.ServiceState, error) {
+	prifiConfig, err := parsePrifi()
+	if err != nil {
+		log.Error("Could not read prifi config")
+		return nil, nil, nil, err
+	}
+
+	host, err := parseCothority()
+	if err != nil {
+		log.Error("Could not start cothority")
+		return nil, nil, nil, err
+	}
+
+	group, err := readCothorityGroupConfig()
+	if err != nil {
+		log.Error("Could not read the group description:", err)
+		return nil, nil, nil, err
+	}
+
+	service := host.Service(prifi_service.ServiceName).(*prifi_service.ServiceState)
+	service.SetConfigFromToml(prifiConfig)
+	prifiConfig.ProtocolVersion = "v1" //getGitCommitID()
+
+	return host, group, service, nil
+}
+
+func parsePrifi() (*prifi_protocol.PrifiTomlConfig, error) {
+	c, err := getPrifiConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	if c.OverrideLogLevel > 0 {
+		log.Lvl3("Log level set to", c.OverrideLogLevel)
+		log.SetDebugVisible(c.OverrideLogLevel)
+	}
+
+	return c, nil
+}
+
+func parseCothority() (*onet.Server, error) {
+	c, err := getCothorityConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	secret, err := crypto.StringHexToScalar(network.Suite, c.Private)
+	if err != nil {
+		return nil, err
+	}
+	point, err := crypto.StringHexToPoint(network.Suite, c.Public)
+	if err != nil {
+		return nil, err
+	}
+
+	serverIdentity := network.NewServerIdentity(point, c.Address)
+	serverIdentity.Description = c.Description
+	server := onet.NewServerTCP(serverIdentity, secret)
+	return server, nil
+}
